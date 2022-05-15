8ed8a6b7dbf02d84ff78f9646ce61bb9a616d041
Andrew Wokabi <drewzote@Andrews-MBP.frontierlocal.net>
2022-05-14 20:41:58 -0700

    Added snakes and ladders to browser game

--- app.js
+++ app.js
@@ -33,13 +33,50 @@ dice.style.top = '500px'
 document.body.append(dice)
 
 
+// Adding Ladders plus positioning them
+let ladder1 = document.createElement('img')
+ladder1.src = './img/81ebbffbfce45b15857b6458408b225d.jpg'
+ladder1.style.position = 'absolute'
+ladder1.style.marginLeft = '390px'
+ladder1.style.marginRight = '450px'
+ladder1.style.top = '207px'
+ladder1.style.opacity = '0.5'
+document.body.append(ladder1)
 
 
+// Adding Ladders plus positioning them
+let ladder2 = document.createElement('img')
+ladder2.src = './img/images.jpg'
+ladder2.style.position = 'absolute'
+ladder2.style.marginLeft = '830px'
+ladder2.style.marginRight = '400px'
+ladder2.style.top = '100px'
+ladder2.style.opacity = '0.5'
+document.body.append(ladder2)
+
+
+// Adding Snakes plus positioning them
+let snake1 = document.createElement('img')
+snake1.src = './img/kisspng-snake-clip-art-purple-snake-5a8d94d64fac23.2785132715192281183263.jpg'
+snake1.style.position = 'absolute'
+snake1.style.marginLeft = '620.8px'
+snake1.style.marginRight = '400px'
+snake1.style.top = '288px'
+snake1.style.opacity = '0.5'
+document.body.append(snake1)
+
+
+let snake2 = document.createElement('img')
+snake2.src = './img/snake-cartoon-png-favpng-t78fjbQvn8CgRCSh9Pj3WaqTz.jpg'
+snake2.style.position = 'absolute'
+snake2.style.marginLeft = '500px'
+snake2.style.marginRight = '400px'
+snake2.style.top = '100px'
+snake2.style.opacity = '0.5'
+document.body.append(snake2)
 
 
 // Waiting for dice roll then move character
-
-
 
 
 
@@ -106,5 +143,4 @@ document.body.append(dice)
     // return {
     //     to: moveToCoordinates,
     //     withArrowKeys: moveWithArrowKeys
-    // }
 
